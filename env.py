ec9d94804b5106676fd75cb6d7ce1db37e761968
Gavin-1001 <gavinshelley10@gmail.com>
2022-06-22 00:06:53 +0100

    Added final wiring of cloudinary and heroku

--- env.py
+++ env.py
@@ -2,3 +2,5 @@ import os
 
 os.environ["DATABASE_URL"] = "postgres://pnoyebfhoqzaad:9510c191361e096b90753b0411f7d8a6b3e56dd3b0cc59677c4f9f1b321821f5@ec2-52-48-159-67.eu-west-1.compute.amazonaws.com:5432/d5anel2vatkp1k"
 os.environ["SECRET_KEY"] = "itsasecret1999!"
+
+os.environ["CLOUDINARY_URL"] = 'cloudinary://227434793138998:srif-DiD4BIvfmWN9pbobYv7G9o@gavin-1001'

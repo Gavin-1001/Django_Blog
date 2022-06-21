4770c0cf3b4893ed10784628e79d94011fe9d6f9
Gavin-1001 <gavinshelley10@gmail.com>
2022-06-21 23:55:28 +0100

    Added project to Heroku, added POSTGRES database to heroku, wired it up

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ["DATABASE_URL"] = "postgres://pnoyebfhoqzaad:9510c191361e096b90753b0411f7d8a6b3e56dd3b0cc59677c4f9f1b321821f5@ec2-52-48-159-67.eu-west-1.compute.amazonaws.com:5432/d5anel2vatkp1k"
+os.environ["SECRET_KEY"] = "itsasecret1999!"

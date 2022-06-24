3b6215fd41b049975f86f610ff487a6807d40ac0
Gavin-1001 <gavinshelley10@gmail.com>
2022-06-24 14:33:33 +0100

    Created New View

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+class PostList(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by('-created_by') ##Only want published posts to be visible to the user hence status=1
+    template_name = 'index.html' # View will render
+    paginate_by: 6 ##Limiting the number of posts that are listed on the front page

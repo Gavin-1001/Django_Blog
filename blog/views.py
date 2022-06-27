4007e1c081a7ecc21339f71004b986d1d3ddfa4d
Gavin-1001 <gavinshelley10@gmail.com>
2022-06-28 00:58:16 +0100

    See "Creating Our First View"

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,6 @@ from .models import Post
 
 class PostList(generic.ListView):
     model = Post
-    queryset = Post.objects.filter(status=1).order_by('-created_by') ##Only want published posts to be visible to the user hence status=1
+    queryset = Post.objects.filter(status=1).order_by('-created_on') ##Only want published posts to be visible to the user hence status=1
     template_name = 'index.html' # View will render
-    paginate_by: 6 ##Limiting the number of posts that are listed on the front page
+    paginate_by = 6 ##Limiting the number of posts that are listed on the front page
--- codestar/settings.py
+++ codestar/settings.py
@@ -18,7 +18,8 @@ if(os.path.isfile('env.py')):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -30,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['gavin-django-blog-2022.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['gavin-django-blog-2022.herokuapp.com', '127.0.0.1', 'localhost']
 
 
 # Application definition

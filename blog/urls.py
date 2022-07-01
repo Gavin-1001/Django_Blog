e0c04a5cf5a04c58b7105cae559fcde8e3c9845b
Gavin-1001 <gavinshelley10@gmail.com>
2022-07-01 13:27:27 +0100

    Can now open a post to read the full post article, created a new view to open post, can also now view comments

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,8 @@
 from . import views
 from django.urls import path
 urlpatterns = [
-    path("", views.PostList.as_view(), name="home")
+    path('', views.PostList.as_view(), name="home"),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    ##1st slug is a path converter, converts text into slug field
+    # 2ndthe second slug is just a keyword name parameter "slug"
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,31 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import get_object_or_404, render
+from django.views import generic, View
 from .models import Post
+
 
 class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by('-created_on') ##Only want published posts to be visible to the user hence status=1
     template_name = 'index.html' # View will render
     paginate_by = 6 ##Limiting the number of posts that are listed on the front page
+    
+    
+class PostDetail(View):
+    
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by("-created_on")
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "liked": liked
+            },
+        )

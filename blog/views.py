12114f83a31168c701fc2b959e23ae7049e3e8ff
Gavin-1001 <gavinshelley10@gmail.com>
2022-07-05 12:27:02 +0100

    Added a post function

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from xml.etree.ElementTree import Comment
 from django.shortcuts import get_object_or_404, render
 from django.views import generic, View
 from .models import Post
@@ -26,6 +27,44 @@ class PostDetail(View):
             {
                 "post": post,
                 "comments": comments,
+                "commented": False, ## tells the user the comment has not been approved
+                "liked": liked,
+                "comment_form": CommentForm()
+            },
+        )
+        
+        
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by("-created_on")
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+            
+        comment_form = CommentForm(data=request.POST) #variable gets data from form
+        
+        if(comment_form.is_valid):
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+        else:
+            comment_form = CommentForm()
+        
+        #pseudo code
+        #if the commment data in the form is valid(i.e all the fields are inputted) then the email,username will be retrieved from the data and set to the username and user. The comment can be posted and is saved in place
+        ## else, if something went wrong with retrieving the data then the form will return an empty comment 
+        
+
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "commented": True, ## tells the user that the comment has been approved and allows others to see it 
                 "liked": liked,
                 "comment_form": CommentForm()
             },

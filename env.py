03af4bbb80fc8729d0c2d2e815e7970c533e510c
Gavin-1001 <gavinshelley10@gmail.com>
2022-07-11 12:57:26 +0100

    Updated env file

--- env.py
+++ env.py
@@ -1,6 +1,6 @@
 import os
 
-os.environ["DATABASE_URL"] = "postgres://pnoyebfhoqzaad:9510c191361e096b90753b0411f7d8a6b3e56dd3b0cc59677c4f9f1b321821f5@ec2-52-48-159-67.eu-west-1.compute.amazonaws.com:5432/d5anel2vatkp1k"
-os.environ["SECRET_KEY"] = "itsasecret1999!"
+os.environ["DATABASE_URL"] = "postgres://ektzbulzhkrzib:288b656dc1c3e2ab043b3ba0d1db2c20ded4544eb72b2a929d3efe70f6017c60@ec2-54-246-185-161.eu-west-1.compute.amazonaws.com:5432/d33po6n7imni2e"
+os.environ["SECRET_KEY"] = "itsasecret1999"
 
 os.environ["CLOUDINARY_URL"] = 'cloudinary://227434793138998:srif-DiD4BIvfmWN9pbobYv7G9o@gavin-1001'
